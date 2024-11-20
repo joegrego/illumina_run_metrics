c2288d99380ab2965a3e12351eaa33499f99217a
Joe Gregoria <joegrego@umich.edu>
2024-11-20 16:10:42 -0500

    add demo data for testing. bug fixes

--- main.py
+++ main.py
@@ -5,10 +5,26 @@ from interop import py_interop_run_metrics, py_interop_run, py_interop_summary
 
 def create_method_dictionary(summary_item):
     summary_dict = {}
-    for method in dir(summary_item):
-        if not method.startswith('_') and method not in ("this", "thisown", "resize") and callable(getattr(summary_item, method)):
-            method = getattr(summary_item, method)
+    for attribute in dir(summary_item):
+        if not attribute.startswith('_') and attribute not in ("this", "thisown", "resize") and callable(getattr(summary_item, attribute)):
+            method = getattr(summary_item, attribute)
             summary_dict[method.__name__] = method()
+
+            if method.__name__.endswith('_pf'):
+                # pf is the Illumina speak for pass-filter.
+                #Things with a pf number usually have a total counterpart, we can calculate a percentage
+                pf_value = method()
+                total_counterpart_name = method.__name__[:-len('_pf')]
+                try:
+                    total_counterpart = getattr(summary_item, total_counterpart_name)
+                    percent_name = f"percent_{method.__name__}"
+                    if percent_name not in summary_dict:
+                        # don't replace a real thing!
+                        summary_dict[percent_name] = (method()/total_counterpart())*100
+                except AttributeError:
+                    # trying and failing is OK, maybe there isn't a counterpart.
+                    pass
+
     return summary_dict
 
 
@@ -17,11 +33,14 @@ def round_floats(the_dict):
     for k, v in the_dict.items():
         if isinstance(v, float):
             new_dict[k] = round(v, 2)
+        else:
+            new_dict[k] = v
     return new_dict
 
 
 if __name__ == '__main__':
-    run_folder = "/Volumes/SharedHITSX/AGC-Tris/Illumina/runs/Next2kA-157/241113_VH00887_157_AAG5FJHM5/"
+    # run_folder = "/Volumes/SharedHITSX/AGC-Tris/Illumina/runs/Next2kA-157/241113_VH00887_157_AAG5FJHM5/"
+    run_folder = "MiSeqDemo/"
 
     run_metrics = py_interop_run_metrics.run_metrics()
     valid_to_load = py_interop_run.uchar_vector(py_interop_run.MetricCount, 0)
@@ -33,20 +52,13 @@ if __name__ == '__main__':
     # print("All the possible values we could display for total_summary:" )
     # print(", ".join([method for method in dir(summary.total_summary()) if not method.startswith('_') and method not in ("this", "thisown", "resize")]))
 
-    # note that there is also a summary.nonindex_summary().*, with I think mostly the same stuff...
-
     total_summary = create_method_dictionary(summary.total_summary())
-    total_summary['percent_reads_pf'] = (summary.total_summary().reads_pf() / summary.total_summary().reads()) * 100
-    total_summary['percent_clusters_pf'] = (summary.total_summary().cluster_count_pf() / summary.total_summary().cluster_count()) * 100
-
     nonindex_summary = create_method_dictionary(summary.nonindex_summary())
-    nonindex_summary['percent_reads_pf'] = (summary.nonindex_summary().reads_pf() / summary.nonindex_summary().reads()) * 100
-    nonindex_summary['percent_clusters_pf'] = (summary.nonindex_summary().cluster_count_pf() / summary.nonindex_summary().cluster_count()) * 100
 
     # round all floats to 2 digits because they can be really long
-    total_summary = round_floats(total_summary)
-    nonindex_summary = round_floats(nonindex_summary)
+    totes = round_floats(total_summary)
+    nonidx = round_floats(nonindex_summary)
 
-    big_summary = {'total_summary': total_summary, 'nonindex_summary': nonindex_summary}
+    everything = {'total_summary': totes, 'nonindex_summary': nonidx}
 
-    print(json.dumps(big_summary, indent=4))
+    print(json.dumps(everything, indent=4))
